94c1833bab8db177a3ce5948b2db4384a0a7c806
@Prakul111 <iamprakul111@gmail.com>
2024-08-30 00:09:23 +0530

    res.write

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const htttp = require('http');
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from Node Server</h1></body>');
+    res.write('</head>');
+    res.end();
+}); 
+
+server.listen(3000);
 
 
 
-
-htttp.createServer((req, res) => {
-    console.log(req);
-});
