ccd4e395911e2a587ba054e584c0d405cb02d19b
@Prakul111 <iamprakul111@gmail.com>
2024-09-03 14:50:45 +0530

    new 01

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
-const http = require('http');
-const bodyParser = require('body-parser');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method"POST"=><input type="text" name="title"><button type="submit">Add Product </button> Product page</form>');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1> hello from express</h1>');
-});
-
 
 app.listen(3000);
-
-
-
-//const server = http.createServer(app);
-//server.listen(3000);
-
-
-
-
-
-
-
-
