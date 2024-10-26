2073fdf8d18db3b32f0f86a09db3ccdcc7b7b7d1
@Prakul111 <iamprakul111@gmail.com>
2024-10-26 21:36:59 +0530

    delete-product

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,11 +2,11 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products',
+      path: '/products'
     });
   });
 };
@@ -14,17 +14,20 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' });
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
-}
-
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/',
+      path: '/'
     });
   });
 };
@@ -39,7 +42,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, product => {
-    Cart.addProducts(prodId, product.price);
+    Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
 };
@@ -57,15 +60,3 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
-
-
-
-
-
-
-
-
-
-
-
-
