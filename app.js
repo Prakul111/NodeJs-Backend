ae09385ba726607fdd23732fa50252f30f08d123
@Prakul111 <iamprakul111@gmail.com>
2025-01-08 23:53:37 +0530

    another

--- app.js
+++ app.js
@@ -37,15 +37,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+
 
 sequelize
   // .sync({ force: true })
